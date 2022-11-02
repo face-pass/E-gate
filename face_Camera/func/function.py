b5fe785dacf8cac2e3973faa1078d33d72d7d965
kazuki13 <92904203+kazuki13@users.noreply.github.com>
2022-11-02 15:20:04 +0900

    Update function.py

--- face_Camera/func/function.py
+++ face_Camera/func/function.py
@@ -3,18 +3,7 @@ import base64
 import json
 import tkinter as tk
 
-def image_to_json(img):
-# ーー　画像を送信可能な形式に変換してJSONに格納　―ー
-  _, encimg = cv2.imencode(".png", img)
-  img_str = encimg.tostring()
-  
-#  ーー base64形式にする　ーー
-  img_byte = base64.b64encode(img_str).decode("utf-8")
 
-#  ーー json形式にする ーー
-  img_json = {'table': 'test','image': img_byte}
-
-  return img_json
 
 def show_window(name):
     root = tk.Tk()
