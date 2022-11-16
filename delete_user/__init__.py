201bb7c7c0ad6fcc0ca7ccd96023baa16385701c
Hoku <hokuto.u0113@gmail.com>
2022-11-16 13:28:12 +0900

    delete function

--- delete_user/__init__.py
+++ delete_user/__init__.py
@@ -6,6 +6,8 @@ from shared_code.azure_blob import Blob
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    file_name = []
+
     # get request
     try:
         json_data = req.get_json()
@@ -13,34 +15,30 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         request_ids = json_data['ids']
         container = json_data['container']
 
-        # debug
-        # logging.info(json_data)
-        # logging.info(table)
-        # logging.info(request_ids)
-
         if not table and request_ids and container:
             logging.error("Error!! Please check request!!")
             return func.HttpResponse("Bad request.", status_code=400)
-        elif request_ids is not list:
+        elif not type(request_ids) == list:
             logging.error("Error!! `request_ids` must be list object")
             return func.HttpResponse("Bad request.", status_code=400)
-        
+              
         # connect Azure service
-        blob = Blob(container)
+        blob = Blob()
         database = MySQL(table)
 
         # get image url
-        image_url = database.getDBImage(request_ids)
+        image_urls = database.getDBImage()
 
+        # get image file name from image_url
+        for image_url in image_urls:
+            split_url = image_url.split("/")
+            file_name.append(split_url[-1])
 
-        # delete blob from above result data
-        blob.delete_image(image_url)
+        # # delete blob from above result data
+        blob.delete_image(container, file_name)
 
-        # success flag
-        logging.info("deleted image")
-
-        # delete user name
-        database.deleteUser(request_ids)
+        # # delete user name
+        database.DeleteUser(request_ids)
 
         return func.HttpResponse("Request Successed!!", status_code=200)
 
