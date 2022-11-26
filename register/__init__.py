4b9face2b8436fa7e7ca65c50e4681f1f227c71a
Hoku <hokuto.u0113@gmail.com>
2022-11-26 10:18:11 +0900

    update register function

--- register/__init__.py
+++ register/__init__.py
@@ -11,20 +11,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # get request form data
     try:
         method = req.method
-        image = req.files.get('image_file')
+        images = req.files.getlist('image_file')
         csv_data = req.files.get('data_file')
         forms = req.form
 
+        logging.info(images)
+        logging.info(csv_data)
+
         # check empty request
-        if not all([image, csv_data, forms]):
+        if not all([images, csv_data, forms]):
             logging.error("Error!! Please check request data")
             return func.HttpResponse("Bad request", status_code=400)            
         
-        # check image file format
-        elif all([image.filename.endswith(".jpg"), image.filename.endswith(".png")]):
-            logging.error("Error!! Please check image file format!! -> image file has supported only 'jpg' or 'png' format")
-            return func.HttpResponse("Bad image file request", status_code=400)
-
         # check data file format    
         elif not csv_data.filename.endswith(".csv"):
             logging.error("Error!! Please check attandance data request!! -> data file has supported only 'csv' or 'xlsx' format")
@@ -42,22 +40,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         logging.info(names)
 
-    # check number of image data equal number of user name
+    # check number of images data equal number of user name
         name_value = len(names)
-        try:
-            image_value = len(image)
-        except:
-            image_list = [image.filename]
-            image_value = len(image_list)
+        image_value = len(images)
 
-        if name_value - 5 != image_value:
+        if name_value != image_value:
             # error response 
             logging.error("You must 'name_value' equal 'image_value' ")
-            return func.HttpResponse("Bad request. You must 'name_value' equal 'image_value'", status_code=400)
+            return func.HttpResponse("Error!! You must 'name_value' equal 'image_value'", status_code=400)
 
     # save image data to blob
         logging.info("started uploading image")
-        blob.save_image_to_blob(image, forms['container'])
+        blob.save_image_to_blob(images, forms['container'])
 
         logging.info("uploaded")
 
@@ -70,8 +64,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         logging.info("started registered")
 
-        for x in range(len(names) - 5):
-            database.Register(names[x], image_url[x])
+        database.Register(names, image_url)
 
         logging.info("registered!!")
         return func.HttpResponse("Registerd user!", status_code=200)
